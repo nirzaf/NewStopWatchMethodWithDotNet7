629133da0ff8208b4c4865a3d6e4e38b64d08d84
nirzaf <mfmfazrin1986@gmail.com>
2022-12-12 23:15:09 +0530

    Initial commit

---
+++ NewStopWatchWithDotNet7/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Stop watch new method in .NET 7");
+
+var startTimer = Stopwatch.GetTimestamp();
+// Do some work
+for(int i = 0; i < 3; i++)
+{
+    await Task.Delay(1000);
+}
+
+
+var diff = Stopwatch.GetElapsedTime(startTimer);
+Console.WriteLine($"Elapsed time: {diff.TotalMilliseconds} ms");
+Console.WriteLine($"Elapsed time: {diff.TotalNanoseconds} ns");
+Console.ReadLine();
